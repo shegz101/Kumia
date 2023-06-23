903fe1e7026cbd0ef56cfb3665678d16eff2e19a
shegz101 <bobatesegun3@gmail.com>
2023-06-23 14:46:59 +0100

    components-created

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.scss";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
+      <Header />
       <h1>Kumia</h1>
     </>
   );
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div>
+      <p>Kumia got you, name it we can get it for you</p>
+    </div>
+  );
+};
+
+export default Header;
