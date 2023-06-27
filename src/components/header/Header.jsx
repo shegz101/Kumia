e36452501b9d331500829c94abc6df3362decdc8
shegz101 <bobatesegun3@gmail.com>
2023-06-27 23:00:38 +0100

    revamped-header-styles

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,28 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Header.css";
 import LogoImage from "../../assets/logo.png";
 import { FiShoppingCart } from "react-icons/fi";
+import { Link } from "react-router-dom";
 import {
   Box,
   Button,
-  ButtonGroup,
   List,
   ListItem,
   Stack,
   UnorderedList,
+  Text,
 } from "@chakra-ui/react";
 
+const cart_count = 0;
+
+const cart_btn = (
+  <Button
+    size="md"
+    bg="#0000ff"
+    color="white"
+    _hover={{ bg: "#6699FF" }}
+    rightIcon={cart_count}
+    gap={1}
+    variant="solid"
+  >
+    <FiShoppingCart />
+  </Button>
+);
+
 const Header = () => {
+  const [activeIndex, setActiveIndex] = useState(0);
+
+  const handleItemClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const items = ["Home", "Contact Us"];
   return (
     <Box
-      w="95%"
+      w={{ base: "98%", lg: "95%" }}
       h="50px"
       display="flex"
       alignItems="center"
       justifyContent="space-between"
-      ml="2.5%"
-      mr="2.5%"
-      backgroundColor=""
+      ml={{ base: "1%", md: "2%", lg: "2.5%" }}
+      mr={{ base: "1%", md: "2%", lg: "2.5%" }}
     >
       {/* Logo */}
       <Box>
@@ -30,11 +53,38 @@ const Header = () => {
       </Box>
       {/* Nav links */}
       <Box>
-        <UnorderedList mt="20px">
+        <UnorderedList
+          mt="20px"
+          display={{ base: "none", lg: "flex" }}
+          justifyContent="center"
+        >
           <List fontWeight="medium">
             <Stack direction="row" spacing={6}>
-              <ListItem>Home</ListItem>
-              <ListItem>Contact Us</ListItem>
+              {items.map((item, index) => (
+                <ListItem
+                  key={index}
+                  position="relative"
+                  onClick={() => handleItemClick(index)}
+                  cursor="pointer"
+                >
+                  <Link to={`/${item.toLowerCase().replace(" ", "-")}`}>
+                    {activeIndex === index && (
+                      <Box
+                        position="absolute"
+                        bottom={0}
+                        left="50%"
+                        transform="translateX(-50%)"
+                        width="100%"
+                        height="2px"
+                        bg="#0000ff"
+                        opacity={0.8}
+                        borderRadius="sm"
+                      />
+                    )}
+                    {item}
+                  </Link>
+                </ListItem>
+              ))}
             </Stack>
           </List>
         </UnorderedList>
@@ -42,33 +92,33 @@ const Header = () => {
       {/* Sign in & Cart */}
       <Box mt="20px">
         {/* cart */}
-        <ButtonGroup>
-          <Box>
-            <Button
-              size="md"
-              bg="rgb(249, 250, 251)"
-              color="white"
-              sx={{
-                "&:hover": {
-                  // Your custom hover styles here
-                  backgroundColor: "rgba(249, 250, 251, 0.5)",
-                },
-              }}
-            >
-              <FiShoppingCart />
-            </Button>
-          </Box>
-          <Box as="span" fontSize="1.0em" verticalAlign="super">
-            0
-          </Box>
+        <Stack direction="row" spacing={4}>
+          <Text mt="10px" fontSize="15px" fontWeight="medium" cursor="pointer">
+            My Orders
+          </Text>
 
-          <Button size="md" bg="#0000ff" color="white">
+          {/* Cart Button */}
+          {cart_btn}
+
+          <Button
+            size="md"
+            bg="#0000ff"
+            color="white"
+            _hover={{ bg: "#6699FF" }}
+          >
             Sign in
           </Button>
-        </ButtonGroup>
+        </Stack>
       </Box>
     </Box>
   );
 };
 
 export default Header;
+
+// sx={{
+//   "&:hover": {
+//     // Your custom hover styles here
+//     backgroundColor: "rgba(249, 250, 251, 0.5)",
+//   },
+// }}
