4852369ca439681cb00a01f4880f072575295090
shegz101 <bobatesegun3@gmail.com>
2023-08-26 14:16:41 +0100

    Search-bar-added-to-mobile-view

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ import LogoImage from "../../assets/logo.png";
 import { FiShoppingCart } from "react-icons/fi";
 import { GiHamburgerMenu } from "react-icons/gi";
 import { Link } from "react-router-dom";
-import { SearchIcon } from "@chakra-ui/icons";
+import { SearchIcon, CloseIcon } from "@chakra-ui/icons";
 import {
   Box,
   Button,
@@ -33,16 +33,16 @@ const cart_btn = (
   </Button>
 );
 
-const HamburgerIcon = ({ onClick }) => (
+const HamburgerIcon = ({ onClick, buttonType }) => (
   <Button
     size="md"
     bg="#0000ff"
     color="#ffffff"
     _hover={{ bg: "#6699FF" }}
-    display={{ base: "block", md: "none" }}
+    display={{ base: "block", lg: "none" }}
     onClick={onClick}
   >
-    <GiHamburgerMenu />
+    {buttonType}
   </Button>
 );
 
@@ -57,70 +57,152 @@ const Header = () => {
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
+    setNavOpen(!navOpen);
   };
+
+  const [navOpen, setNavOpen] = useState(false);
+
   //create an array of the navs
   const navs = ["Home", "Contact Us"];
   return (
     <Box
       w={"100%"}
-      h="70px"
+      h={{ base: "max-content", md: "80px" }}
       display="flex"
-      alignItems="center"
-      alignContent={"center"}
+      flexDirection="column"
       justifyContent="space-between"
-      pl={{ base: "1%", md: "2%", lg: "2.5%" }}
-      pr={{ base: "1%", md: "2%", lg: "2.5%" }}
-      pt="20px"
-      pb="20px"
+      pl={{ base: "1.5%", md: "2%", lg: "2.5%" }}
+      pr={{ base: "1.5%", md: "2%", lg: "2.5%" }}
+      pt="8px"
+      pb="8px"
       boxShadow="md"
       bg="#FFFFFF"
     >
-      {/* Logo */}
-      <Box mb={"20px"}>
-        <img src={LogoImage} alt="logo" />
-      </Box>
+      {/* Box 1 */}
+      <Box
+        w={"100%"}
+        h="70px"
+        display="flex"
+        alignItems="center"
+        alignContent={"center"}
+        justifyContent="space-between"
+      >
+        {/* Logo */}
+        <Box mb={"20px"}>
+          <img src={LogoImage} alt="logo" />
+        </Box>
 
-      {/* Nav links */}
-      <Box>
-        <UnorderedList
-          display={{ base: "none", lg: "flex" }}
-          justifyContent="center"
+        {/* Nav links */}
+        <Box>
+          <UnorderedList
+            display={{ base: "none", lg: "flex" }}
+            justifyContent="center"
+          >
+            <List fontWeight="medium">
+              <Stack direction="row" spacing={10}>
+                {/* dynamically mapped through the navs */}
+                {navs.map((nav, index) => (
+                  <ListItem
+                    key={index}
+                    position="relative"
+                    onClick={() => handleItemClick(index)}
+                    cursor="pointer"
+                  >
+                    <Link to={`/${nav.toLowerCase().replace(" ", "-")}`}>
+                      {activeIndex === index && (
+                        <Box
+                          position="absolute"
+                          bottom={0}
+                          left="50%"
+                          transform="translateX(-50%)"
+                          width="100%"
+                          height="2px"
+                          bg="#0000ff"
+                          opacity={0.8}
+                          borderRadius="sm"
+                        />
+                      )}
+                      {nav}
+                    </Link>
+                  </ListItem>
+                ))}
+              </Stack>
+            </List>
+          </UnorderedList>
+        </Box>
+
+        {/* Search box */}
+        <Box display={{ base: "none", md: "block" }}>
+          <InputGroup>
+            <Input
+              type="text"
+              placeholder="Search for a Product"
+              borderRadius="2xl"
+              focusBorderColor="#F5F5F5"
+              outline="none"
+              bg={"#F5F5F5"}
+              w="350px"
+            />
+            <InputRightElement pointerEvents="none">
+              <SearchIcon color="black.200" />
+            </InputRightElement>
+          </InputGroup>
+        </Box>
+
+        {/* Sign in, Cart & Hamburger Icon */}
+        <Box>
+          {/* cart */}
+          <Stack direction="row" spacing={4}>
+            {/* Cart Button */}
+            {cart_btn}
+
+            {/* Sign in Button */}
+            <Button
+              size="md"
+              bg="#0000ff"
+              color="white"
+              display={{ base: "none", lg: "block" }}
+              _hover={{ bg: "#6699FF" }}
+            >
+              Sign in
+            </Button>
+
+            {/* Hamburger Icon */}
+            <HamburgerIcon
+              onClick={toggleMenu}
+              buttonType={navOpen ? <CloseIcon /> : <GiHamburgerMenu />}
+            />
+          </Stack>
+        </Box>
+        {/* Mobile Menu Overlay */}
+        <Box
+          className={`mobile-menu-overlay ${isMenuOpen ? "active" : ""}`}
+          display={{
+            base: isMenuOpen ? "block" : "none",
+            lg: "none",
+          }}
+          top={{ base: "130px", md: "80px" }}
+          right="0"
+          width="50%"
+          height={`calc(100% - 130px)`}
+          position="fixed"
+          bg={"#F5F5F5"}
+          zIndex="2"
+          overflowY="auto"
+          transition="right 0.3s ease-in-out"
         >
-          <List fontWeight="medium">
-            <Stack direction="row" spacing={10}>
-              {/* dynamically mapped through the navs */}
-              {navs.map((nav, index) => (
-                <ListItem
-                  key={index}
-                  position="relative"
-                  onClick={() => handleItemClick(index)}
-                  cursor="pointer"
-                >
-                  <Link to={`/${nav.toLowerCase().replace(" ", "-")}`}>
-                    {activeIndex === index && (
-                      <Box
-                        position="absolute"
-                        bottom={0}
-                        left="50%"
-                        transform="translateX(-50%)"
-                        width="100%"
-                        height="2px"
-                        bg="#0000ff"
-                        opacity={0.8}
-                        borderRadius="sm"
-                      />
-                    )}
-                    {nav}
-                  </Link>
-                </ListItem>
-              ))}
-            </Stack>
-          </List>
-        </UnorderedList>
+          <div className="mobile-menu-content">
+            <Link to="/">Home</Link>
+            <Link to="/contact-us">Contact Us</Link>
+            <Button size="md" bg="#0000ff" color="white">
+              Sign in
+            </Button>
+          </div>
+        </Box>
       </Box>
 
-      {/* Search box */}
-      <Box display={{ base: "none", md: "block" }}>
+      {/* Box 2 */}
+      <Box display={{ base: "block", md: "none" }}>
         <InputGroup>
           <Input
             type="text"
@@ -129,54 +211,14 @@ const Header = () => {
             focusBorderColor="#F5F5F5"
             outline="none"
             bg={"#F5F5F5"}
-            w="350px"
+            w="100vw"
+            ml="1.5%"
+            mr="1.5%"
           />
           <InputRightElement pointerEvents="none">
             <SearchIcon color="black.200" />
           </InputRightElement>
         </InputGroup>
-      </Box>
-      {/* Sign in & Cart */}
-      <Box>
-        {/* cart */}
-        <Stack direction="row" spacing={4}>
-          {/* Cart Button */}
-          {cart_btn}
-
-          <Button
-            size="md"
-            bg="#0000ff"
-            color="white"
-            display={{ base: "none", md: "block" }}
-            _hover={{ bg: "#6699FF" }}
-          >
-            Sign in
-          </Button>
-
-          <HamburgerIcon onClick={toggleMenu} />
-        </Stack>
-      </Box>
-      {/* Mobile Menu Overlay */}
-      <Box
-        className={`mobile-menu-overlay ${isMenuOpen ? "active" : ""}`}
-        display={{ base: isMenuOpen ? "block" : "none", md: "none" }}
-        top="72px"
-        right="0"
-        width="50%"
-        height={`calc(100% - 70px)`}
-        position="fixed"
-        bg={"#F5F5F5"}
-        zIndex="2"
-        overflowY="auto"
-        transition="right 0.3s ease-in-out"
-      >
-        <div className="mobile-menu-content">
-          <Link to="/">Home</Link>
-          <Link to="/contact-us">Contact Us</Link>
-          <Button size="md" bg="#0000ff" color="white">
-            Sign in
-          </Button>
-        </div>
       </Box>
     </Box>
   );
