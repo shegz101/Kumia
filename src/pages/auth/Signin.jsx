bd7c35da2b36b1bdf94eae60430ec41a048a0684
Bobate Olusegun <bobatesegun3gmail.com>
2023-09-08 06:58:51 +0100

    sign-in-successful

--- src/pages/auth/Signin.jsx
+++ src/pages/auth/Signin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { FcGoogle } from "react-icons/fc";
 import { FaGithub, FaTwitter } from "react-icons/fa";
 import {
@@ -14,6 +14,12 @@ import {
 } from "@chakra-ui/react";
 import { ViewIcon, ViewOffIcon, LockIcon, EmailIcon } from "@chakra-ui/icons";
 import loginImg from "../../assets/login.png";
+import { Loader } from "../../components";
+import { signInWithEmailAndPassword } from "firebase/auth";
+import { auth } from '../../firebase/firebaseconfig';
+// import toastify
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Card = ({ cardContent }) => (
   <Box
@@ -27,12 +33,12 @@ const Card = ({ cardContent }) => (
   </Box>
 );
 
-const Content = ({ onclick, show }) => (
+const Content = ({ onclick, show, email, setEmail, password, setPassword, signIn }) => (
   <Stack alignItems="center" display="flex" flexDirection="column" spacing={3}>
     <Text color="#0000ff" fontSize={25} fontWeight="semibold">Login</Text>
 
     <InputGroup size="lg">
-      <Input placeholder="janehop@gmail.com"  type="email" />
+      <Input placeholder="janehop@gmail.com"  type="email" value={email}  onChange={(e) => setEmail(e.target.value)}/>
 
       <InputLeftElement pointerEvents='none' size={6}>
         <EmailIcon color='gray.300' />
@@ -44,6 +50,8 @@ const Content = ({ onclick, show }) => (
         w="400px"
         type={show ? "text" : "password"}
         placeholder="Enter password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
       />
 
       <InputLeftElement pointerEvents='none' size={6}>
@@ -57,7 +65,7 @@ const Content = ({ onclick, show }) => (
       </InputRightElement>
     </InputGroup>
 
-    <Button w={{base:"100%", lg:"400px"}} color="white" backgroundColor="#0000ff" _hover={{ bg: "#6699FF" }}>
+    <Button w={{base:"100%", lg:"400px"}} color="white" backgroundColor="#0000ff" _hover={{ bg: "#6699FF" }} onClick={signIn}>
       Login
     </Button>
 
@@ -84,21 +92,60 @@ const Content = ({ onclick, show }) => (
 const Signin = () => {
   const [show, setShow] = useState(false);
   const handleClick = () => setShow(!show);
+
+  // state for the input fields
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  // track loading state
+  const [isLoading, setIsloading] = useState(false);
+
+  const navigate = useNavigate();
+
+  //function to sign in users
+  const signIn = (e) => {
+    e.preventDefault();
+    if (email === "" || password === "") {
+      toast.error("Please fill the input fields!")
+    }
+    setIsloading(true);
+    console.log(email, password);
+
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        const user = userCredential.user;
+        setIsloading(false);
+        toast.success("sign in successful!");
+        navigate("/");
+      })
+      .catch((error) => {
+        const errorMessage = error.message;
+        toast.error({errorMessage});
+        setIsloading(false);
+      });
+    setEmail("");
+    setPassword("");
+  }
+
   return (
-    <Box
+    <>
+      <ToastContainer />
+      {isLoading && <Loader/>}
+      <Box
       display="flex"
       justifyContent="center"
       alignItems="center"
       marginTop="20px"
       paddingLeft="50px"
       paddingRight="50px"
-    >
-      <Box display={{base: "none", md:"block"}}>
-        <img src={loginImg} alt="login preset" style={{ width: "500px" }} />
-      </Box>
+      >
+        <Box display={{base: "none", md:"block"}}>
+          <img src={loginImg} alt="login preset" style={{ width: "500px" }} />
+        </Box>
 
-      <Card cardContent={<Content onclick={handleClick} show={show} />} />
-    </Box>
+        <Card cardContent={<Content onclick={handleClick} show={show} password={password} email={email} setEmail={setEmail} setPassword={setPassword} signIn={signIn} />} />
+      </Box>
+    </>
   );
 }
 
