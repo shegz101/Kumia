612fb3e4a60908ff073299ba13f9c7b4ae726954
shegz101 <bobatesegun3@gmail.com>
2023-09-12 22:43:41 +0100

    linked-auth-reducer-to-store

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,10 @@
 // import the configure store
 import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import authReducer from "./features/authSlice";
 
-const rootReducer = combineReducers();
+const rootReducer = combineReducers({
+    auth: authReducer,
+});
 
 const store = configureStore({
     reducer: rootReducer,
