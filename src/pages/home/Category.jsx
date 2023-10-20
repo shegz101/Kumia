ab5cd5567f1b1e4ac84aae58effc39065f430302
shegz101 <bobatesegun3@gmail.com>
2023-10-20 19:48:39 +0100

    added-all-categories

--- src/pages/home/Category.jsx
+++ src/pages/home/Category.jsx
@@ -1,18 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Box, Stack, Text } from "@chakra-ui/react";
+import { RiArrowDropRightLine } from "react-icons/ri";
 
 const Category = () => {
+  const categories = ["All", "Mobile Phones", "Cameras", "Laptops", "Headsets"];
+  const [leftBorderCategoryTracker, setLeftBorderCategoryTracker] = useState(0);
   return (
-    <Box pt={5}>
+    <Box pt={5} display={"flex"} flexDirection={"row"} 
+    width={"70%"} margin={"0 auto"}>
+      {/* Product Categories */}
+      <Box width={"15%"}>
         <Text color="#0000ff" fontSize={"2xl"} 
-        textAlign={"center"} fontWeight={"medium"}>
-            Categories
+        fontWeight={"medium"}>
+              Categories
         </Text>
+        {/* All Categories */}
+        <Stack>
+          {
+            categories.map((a_category, id) => (
+              <button key={id} style={{ display:"flex", flexDirection:"row", textAlign: "start", height:"30px", paddingTop:"5px", 
+              borderBottom:"1px solid black", borderLeft:"3px solid #0000ff"}}>
+                <span><RiArrowDropRightLine style={{ marginTop:"3px"}}/></span> {a_category}
+              </button>
+            ))
+          }
+        </Stack>
+      </Box>
+
+      {/* Products cards */}
+      <Box width={"65%"}>
         {/* The Various Categories */}
         <Stack>
           {/* Laptops & Computer Category  */}
-           
+          <Text>Product Card</Text>
         </Stack>
+      </Box>
     </Box>
   )
 }
